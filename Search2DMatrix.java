f2958febc7ff5fd4e90c4e1cabe110772fa70748
Rosy999k <arya999k@gmail.com>
2021-12-30 15:15:10 -0600

    Imagine the matrix as a sorted array and implement binary search

---
+++ Search2DMatrix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix.length == 0) return false;
+        int left = 0, rows = matrix.length, cols = matrix[0].length;
+        int right = rows * cols -1;int pivot = 0;
+        while(left <= right) {
+            int mid = (left+right)/2;
+            pivot = matrix[mid/cols][mid%cols];
+            if(pivot == target) return true;
+            if(pivot < target) {
+                left = mid+1;
+            }
+            else
+            {
+                right =mid-1;
+            }
+        }
+        return false;
+    }
+}
