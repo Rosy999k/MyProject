f0c2a0d63ad397b849f98d482aee4f1c80024bc1
Rosy999k <arya999k@gmail.com>
2022-01-02 18:01:44 -0600

    traverse through all colors compare i and i+1

--- MinimumTimeToMakeRopeColorful.java
+++ MinimumTimeToMakeRopeColorful.java
@@ -1,8 +1,8 @@
 class Solution {
     public int minCost(String colors, int[] neededTime) {
-        // int n = colors.length();
+         int n = colors.length();
         int result = 0;
-        for(int i = 0; i<colors.length()-1;i++){
+        for(int i = 0; i<n-1;i++){
             if(colors.charAt(i) == colors.charAt(i+1)) {
                 result = result+ Math.min(neededTime[i], neededTime[i+1]);
                 neededTime[i+1]=Math.max(neededTime[i], neededTime[i+1]);
